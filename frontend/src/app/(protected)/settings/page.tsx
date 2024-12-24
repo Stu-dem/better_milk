27fcf31c52547b2ad1e3bf7a7d56463e2781d632
Stuart Demmer <dstuart99@gmail.com>
2024-12-24 04:33:22 +0200

    Refactor settings page: enhance UI with Card component and improve cookie display

--- frontend/src/app/(protected)/settings/page.tsx
+++ frontend/src/app/(protected)/settings/page.tsx
@@ -1,22 +1,26 @@
 "use server";
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 const SettingsPage = async () => {
-
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
   return (
-    <div>
-      <h1>Settings</h1>
-      {
-        cookieStore.getAll().map((cookie) => (
-          <div key={cookie.name}>
-            <p>Name: {cookie.name}</p>
-            <p>Value: {cookie.value}</p>
+    <Card className="w-[600] shadow-md">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">Settings</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        {cookieStore.getAll().map((cookie) => (
+          <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+            <p className="text-sm font-medium">{cookie.name}</p>
+            <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+              {cookie.value}
+            </p>
           </div>
-        ))
-      }
-    </div>
+        ))}
+      </CardContent>
+    </Card>
   );
 };
 
